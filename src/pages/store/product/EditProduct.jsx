792893198c309f9ddf1dd99a83746d1f1742d1ff
vanphuoc <phuoc0905886611@gmail.com>
2024-04-04 01:54:44 +0700

    layout crud product

--- src/pages/store/product/EditProduct.jsx
+++ src/pages/store/product/EditProduct.jsx
@@ -1,17 +1,17 @@
 import { IoMdClose } from "react-icons/io";
 
-const AddProduct = (props) => {
-    function handleAddProduct(e) {
+const EditProduct = (props) => {
+    function handleEditProduct(e) {
         e.preventDefault();
         // Add your login logic here (e.g., API calls, validation)
         props.toggle(); // Close the popup form
     }
     return (
 
-        <div className=" w-full fixed top-0 left-0 z-50 items-center justify-center " onSubmit={handleAddProduct}>
+        <div className=" w-full fixed top-0 left-0 z-50 items-center justify-center " onSubmit={handleEditProduct}>
             <div className="w-[50%] fixed top-[25%] left-[30%] bg-[#f5f0f0] rounded-lg shadow-2xl flex">
                 <form className=" w-[70%] py-[30px] grid justify-items-center  ">
-                    <h2 className="text-3xl font-semibold mb-4 text-black">Add Product</h2>
+                    <h2 className="text-3xl font-semibold mb-4 text-black">Edit Product</h2>
 
                     <div class="mb-4 w-[80%] text-black">
                         <input type="text" placeholder="CategoryName" className="input" />
@@ -29,7 +29,7 @@ const AddProduct = (props) => {
                     <div class="mb-4 w-[80%] text-black">
                         <input type="text" placeholder="Description" className="input " />
                     </div>
-                    <button type="submit" className="btn">Create</button>
+                    <button type="submit" className="btn">Update</button>
                 </form>
                 <div className="w-[50%] bg-blue-700 flex items-center justify-center clip-polygon rounded-md">
                     <img className="w-full" src="https://p.w3layouts.com/demos_new/template_demo/06-10-2021/grocery-mart-liberty-demo_Free/794674028/web/images/banner-img.png" alt="" />
@@ -45,4 +45,4 @@ const AddProduct = (props) => {
     )
 }
 
-export default AddProduct;
+export default EditProduct;
--- src/pages/store/product/ListProducts.jsx
+++ src/pages/store/product/ListProducts.jsx
@@ -1,24 +1,31 @@
 import React, { useState } from 'react';
-
 import { AgGridReact } from 'ag-grid-react'; // AG Grid Component
 import "ag-grid-community/styles/ag-grid.css"; // Mandatory CSS required by the grid
 import "ag-grid-community/styles/ag-theme-quartz.css"; // Optional Theme applied to the grid
 import AddProduct from './AddProduct';
+import EditProduct from './EditProduct';
 
 const CustomButtonComponent = (props) => {
+
     return (
-        <div>
-            <button className='bg-yellow-400 px-4 rounded-lg ' onClick={() => window.alert('Edit') }>Edit</button>
+        <>
+        <div className=''>
+            <button className='bg-yellow-400 px-4 rounded-lg ' onClick={props.customProp}>Edit</button>
             <button className='bg-red-600 px-4 rounded-lg ' onClick={() => window.alert('Edit') }>Delete</button>
         </div>
+        </>
     )
   };
 
 const ListProduct = () => {
-    const [seen, setSeen] = useState(false);
+    const [add, setAdd] = useState(false);
+    const [edit, setEdit] = useState(false);
 
-    function togglePop() {
-        setSeen(!seen);
+    function toggleAddProduct() {
+        setAdd(!add);
+    }
+    function toggleEditProduct() {
+        setEdit(!edit);
     }
 
     const [rowData, setRowData] = useState([
@@ -26,7 +33,7 @@ const ListProduct = () => {
         { id: "2", Category: "Meats",  Name : "qweqweqweq", Price: 200, Description: "adadasdadasdasdada" },
         { id: "3", Category: "Vegetables", Name : "Seaqweqwqweqwfood", Price: 200, Description: "adadasdadasdasdada" },
     ]);
-
+    
     // Column Definitions: Defines the columns to be displayed.
     const [colDefs, setColDefs] = useState([
         { field: "id", flex: 1 },
@@ -34,7 +41,9 @@ const ListProduct = () => {
         { field: "Name", flex: 1 },
         { field: "Price", flex: 1 },
         { field: "Description", flex: 1 },
-        { field: "action", cellRenderer: CustomButtonComponent, flex: 1 }
+        {     field: "action", 
+        cellRenderer: (params) => <CustomButtonComponent {...params} customProp={toggleEditProduct} />, 
+        flex: 1 }
     ]);
 
     return (
@@ -43,11 +52,11 @@ const ListProduct = () => {
         >
             <div className="mb-4">
                 <h1 className="text-center text-3xl text-white py-4">List Products</h1>
-                <button className="bg-blue-600 hover:bg-blue-700 px-4 py-2 rounded-lg text-white " onClick={togglePop}>Add Product</button>
+                <button className="bg-blue-600 hover:bg-blue-700 px-4 py-2 rounded-lg text-white " onClick={toggleAddProduct}>Add Product</button>
             </div>
-            {seen ? <AddProduct toggle={togglePop} /> : null}
-
-            <AgGridReact className='w-[90%] mx-auto z-10'
+            {add ? <AddProduct toggle={toggleAddProduct} /> : null}
+            {edit ? <EditProduct toggle={toggleEditProduct} /> : null}
+            <AgGridReact className='w-[90%] mx-auto z-[0]'
                 rowData={rowData}
                 columnDefs={colDefs}
             />
