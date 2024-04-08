e4f73b57e62f831f104d4c3aec289cf99ea1f3db
vanphuoc <phuoc0905886611@gmail.com>
2024-04-08 21:13:19 +0700

    Layout Store

--- src/pages/store/home/SidebarStore.jsx
+++ src/pages/store/home/SidebarStore.jsx
@@ -1,30 +1,41 @@
-import { MdOutlineShoppingCart, MdDashboard, MdLogout  } from "react-icons/md";
-import { PiUserListBold } from "react-icons/pi";
-import { FaStore } from "react-icons/fa";
+import { MdOutlineShoppingCart, MdDashboard, MdLogout, MdFeedback   } from "react-icons/md";
+import { IoMdChatboxes } from "react-icons/io";
 import { BsFillArchiveFill } from "react-icons/bs";
 import { useNavigate } from 'react-router-dom';
 
 const SidebarStore = () => {
     const navigate = useNavigate();
     return (
-        <div className="">
+        <div className="flex justify-around">
             <h3 className="flex text-3xl py-4 items-center justify-center">
                 <MdOutlineShoppingCart className="mr-2" />
                 K Mart</h3>
-            <ul className="pl-6 max-md:grid max-md:justify-items-center">
-                <li className="item-adminBar ">
+            <ul className="pl-6 flex">
+                <li className="item-adminBar px-2">
                     <MdDashboard className="mr-2" />
                     <button onClick={() => { navigate('') }} className="text-lg">
                         Dashboard
                     </button>
                 </li>
-                <li className="item-adminBar ">
+                <li className="item-adminBar px-2">
                     <BsFillArchiveFill className="mr-2" />
                     <button onClick={() => { navigate('listproduct') }} className="text-lg">
                         Products
                     </button>
                 </li>
-                <li className="item-adminBar ">
+                <li className="item-adminBar px-2">
+                    <IoMdChatboxes className="mr-2" />
+                    <button onClick={() => { navigate('message') }} className="text-lg">
+                        Message
+                    </button>
+                </li>
+                <li className="item-adminBar px-2">
+                    <MdFeedback  className="mr-2" />
+                    <button onClick={() => { navigate('feedback') }} className="text-lg">
+                        Feedback
+                    </button>
+                </li>
+                <li className="item-adminBar px-2">
                     <MdLogout  className="mr-2" />
                     <button onClick={() => { navigate('') }} className="text-lg">
                         Logout
--- src/pages/store/home/Store.jsx
+++ src/pages/store/home/Store.jsx
@@ -4,11 +4,11 @@ import SidebarStore from "./SidebarStore";
 const Store = () => {
 
     return (
-        <div className="md:flex lg:h-screen ">
-            <div className="bg-gray-800 text-slate-400 w-full md:w-[30%] xl:w-[15%]">
+        <div className=" h-screen">
+            <div className="bg-gray-800 text-slate-400 w-full h-[7%] ">
             <SidebarStore/>
             </div>
-            <div className="bg-gray-950 text-white w-full px-6 md:w-[70%] xl:w-[85%]">
+            <div className="bg-gray-950 text-white w-full ">
                 <Outlet />
             </div>
         </div>
